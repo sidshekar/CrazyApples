4ea773b170162ff7023b8bdbe8d8b45ef2c24ada
siddharth shekar <siddharthshekar@siddharths-Mac-Pro.local>
2012-05-18 12:09:42 +0530

    Initial Commit

---
+++ cocos2d_temp/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  cocos2d_temp
+//
+//  Created by siddharth shekar on 5/18/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ cocos2d_temp/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  cocos2d_temp
+//
+//  Created by siddharth shekar on 5/18/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
